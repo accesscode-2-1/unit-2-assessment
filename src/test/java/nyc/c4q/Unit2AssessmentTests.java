b59a050490df4fdf0193cfd6f606bb3a1cdf6696
John Rodriguez <john.rodriguez@gmail.com>
2015-06-22 02:50:39 -0400

    update unit tests

--- src/test/java/nyc/c4q/Unit2AssessmentTests.java
+++ src/test/java/nyc/c4q/Unit2AssessmentTests.java
@@ -3,6 +3,7 @@ package nyc.c4q;
 import android.app.Activity;
 import android.widget.TextView;
 
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +11,7 @@ import org.junit.runners.MethodSorters;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import static org.junit.Assert.assertTrue;
+import org.robolectric.util.ActivityController;
 
 //import static org.hamcrest.MatcherAssert.assertThat;
 //import static org.hamcrest.Matchers.equalTo;
@@ -23,11 +23,21 @@ import static org.assertj.android.api.Assertions.assertThat;
 @Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
 public class Unit2AssessmentTests {
 
+    private ActivityController<Unit2AssessmentActivity> activityController;
+    private Activity activity;
+
+    @Before
+    public void setUp() {
+        activityController = Robolectric.buildActivity(Unit2AssessmentActivity.class);
+        activity = activityController.get();
+    }
+
     @Test
-    public void testSomething() throws Exception {
-        Activity a = Robolectric.buildActivity(Unit2AssessmentActivity.class).create().get();
-        TextView tv = (TextView) a.findViewById(R.id.text);
-        assertTrue(a != null);
+    public void testSomething() {
+        activityController.create();
+        assertThat(activity).isNotNull();
+
+        TextView tv = (TextView) activity.findViewById(R.id.text);
         assertThat(tv).isNotVisible();
 //        assertThat(5, equalTo(3));
 //        assertThat(view).isGone();
