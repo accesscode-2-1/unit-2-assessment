2521dead5e8ded8e3c48eed69d006488f27b8311
ramonaharrison <ramona.e.harrison@gmail.com>
2015-06-28 15:07:07 -0400

    finished 3 + 4

--- src/main/java/nyc/c4q/NotificationActivity.java
+++ src/main/java/nyc/c4q/NotificationActivity.java
@@ -1,8 +1,10 @@
 package nyc.c4q;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class NotificationActivity extends Activity {
@@ -24,6 +26,57 @@ public class NotificationActivity extends Activity {
         Button permanentnotification = (Button) findViewById(R.id.permanentnotification);
         Button dismisspermanentnotification = (Button) findViewById(R.id.dismisspermanentnotification);
         Button buttonnotification = (Button) findViewById(R.id.buttonnotification);
+
+        autocancelnotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification.Builder mBuilder =
+                        new Notification.Builder(getBaseContext())
+                                .setSmallIcon(R.drawable.c4qfavicon)
+                                .setContentTitle("default@c4q.nyc")
+                                .setContentText("Touch me to dismiss me!");
+                Notification n = mBuilder.build();
+                n.flags = Notification.FLAG_AUTO_CANCEL;
+                notificationManager.notify(0, n);
+
+            }
+        });
+
+        swipenotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification.Builder mBuilder =
+                        new Notification.Builder(getBaseContext())
+                                .setSmallIcon(R.drawable.c4qfavicon)
+                                .setContentTitle("swipe@c4q.nyc")
+                                .setContentText("Swipe right if you want to meet me. Otherwise, I'm not going away.");
+                Notification n = mBuilder.build();
+                n.flags = 0;
+                notificationManager.notify(1, n);
+            }
+        });
+
+        permanentnotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification.Builder mBuilder =
+                        new Notification.Builder(getBaseContext())
+                                .setSmallIcon(R.drawable.c4qfavicon)
+                                .setOngoing(true)
+                                .setContentTitle("permanent@c4q.nyc")
+                                .setContentText("I'm staying planted right here.");
+                Notification n = mBuilder.build();
+                notificationManager.notify(2, n);
+            }
+        });
+
+        dismisspermanentnotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notificationManager.cancel(2);
+            }
+        });
 
     }
 }
