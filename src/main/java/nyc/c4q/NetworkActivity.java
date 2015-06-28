056a18a50b384295508ddfdfe05f2eb6a09dc144
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-06-28 12:06:11 -0400

    Partially working listview and network activity

--- src/main/java/nyc/c4q/NetworkActivity.java
+++ src/main/java/nyc/c4q/NetworkActivity.java
@@ -4,25 +4,18 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.HttpUrl;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-
-import java.io.BufferedInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class NetworkActivity extends Activity {
 
@@ -35,6 +28,7 @@ public class NetworkActivity extends Activity {
     public Button httpbinpostokhttp;
     public Button cleartextlog;
     final public String urlParams = "custname=james+dean&custtel=347-841-6090&custemail=hello%40c4q.nyc&size=small&topping=cheese&delivery=18%3A15&comments=Leave+it+by+the+garage+door.+Don't+ask+any+questions.";
+    public AsyncTask asyncHttps;
 
     // Code ===========================
 
@@ -76,6 +70,12 @@ public class NetworkActivity extends Activity {
         httpbinget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(asyncHttps == null){
+                    asyncHttps = new AsyncHttps().execute();
+                }else if(!asyncHttps.isCancelled()){
+                    asyncHttps.cancel(true);
+                    asyncHttps = new AsyncHttps().execute();
+                }
             }
         });
 
@@ -103,5 +103,39 @@ public class NetworkActivity extends Activity {
                 httptextlog.setText("cleared HTTP response");
             }
         });
+    }
+
+    public class AsyncHttps extends AsyncTask<Void,Void,String>{
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String httpbin = null;
+
+            try {
+                URL url = new URL("https://httpbin.org/get?" + urlParams);
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                InputStream in = httpsURLConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder builder = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+                httpbin = builder.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return httpbin;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            httptextlog.setText(s);
+        }
     }
 }
