98a0ecc6eb183823808ccc5cdbcc97fc500ee227
eboves <eboves@gmail.com>
2015-06-28 16:43:49 -0400

    what I have so far

--- src/main/java/nyc/c4q/JSONActivity.java
+++ src/main/java/nyc/c4q/JSONActivity.java
@@ -3,6 +3,7 @@ package nyc.c4q;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -13,15 +14,25 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 import nyc.c4q.json.Zipcode;
+import nyc.c4q.json.ZipcodeDeserializer;
 
 public class JSONActivity extends Activity {
 
@@ -36,7 +47,7 @@ public class JSONActivity extends Activity {
         zipcodes = new ArrayList<Zipcode>();
 
         Button savejson = (Button) findViewById(R.id.savejson);
-        Button loadjson = (Button) findViewById(R.id.loadjson);
+        final Button loadjson = (Button) findViewById(R.id.loadjson);
         final Button addjson = (Button) findViewById(R.id.addjson);
 
         final TextView _id = (TextView) findViewById(R.id.field_idvalue);
@@ -46,13 +57,14 @@ public class JSONActivity extends Activity {
         final TextView _lat = (TextView) findViewById(R.id.fieldloclatvalue);
         final TextView _long = (TextView) findViewById(R.id.fieldloclongvalue);
 
+        //_id.setText(zipcodes.hashCode());
+
 
 
 
         addjson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            addJson();
 
             }
         });
@@ -62,6 +74,18 @@ public class JSONActivity extends Activity {
             public void onClick(View v) {
                 File directory = getExternalCacheDir();
                 File file = new File(directory, "zipcodes.json");
+
+
+                ObjectOutputStream outputStream = null;
+                try{
+                    outputStream = new ObjectOutputStream(new FileOutputStream(file));
+                    System.out.println("Start Writings");
+                    outputStream.writeObject(file);
+                    outputStream.flush();
+                    outputStream.close();
+                }catch (Exception e){
+                    System.err.println("Error: " + e);
+                }
             }
         });
 
@@ -71,14 +95,8 @@ public class JSONActivity extends Activity {
             public void onClick(View v) {
                 File directory = getExternalCacheDir();
                 File file = new File(directory, "zipcodes.json");
+
             }
         });
     }
-
-    private void addJson() {
-
-
-
-    }
-
 }
