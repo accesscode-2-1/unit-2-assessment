ef54ce446693eb6c3fc1086edc1869f2099bcbbf
rayacevdo45 <rayacevedo45@gmail.com>
2015-06-28 12:59:26 -0400

    2nd commit

--- src/main/java/nyc/c4q/NotificationActivity.java
+++ src/main/java/nyc/c4q/NotificationActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class NotificationActivity extends Activity {
@@ -19,15 +20,15 @@ public class NotificationActivity extends Activity {
         setContentView(R.layout.activity_notification);
 
 
-        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification.Builder builder = new  Notification.Builder(NotificationActivity.this);
-        builder.setContentTitle("notification");
-        //builder.setContentText("notification");
-        //builder.setSmallIcon(R.drawable.ic_launcher);
-
-        //builder.setContentIntent(pendingIntent);
-        Notification notification = builder.build();
-        notificationManager.notify(1, notification);
+//        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        Notification.Builder builder = new  Notification.Builder(NotificationActivity.this);
+//        builder.setContentTitle("default@c4q.nyc");
+//        builder.setContentText("Touch me to dismiss me!");
+//        builder.setSmallIcon(R.drawable.c4qfavicon);
+//        builder.setAutoCancel(true);
+//
+//        Notification notification = builder.build();
+//        notificationManager.notify(1, notification);
 
         Button autocancelnotification = (Button) findViewById(R.id.autocancelnotification);
         Button swipenotification = (Button) findViewById(R.id.swipenotification);
@@ -35,5 +36,63 @@ public class NotificationActivity extends Activity {
         Button dismisspermanentnotification = (Button) findViewById(R.id.dismisspermanentnotification);
         Button buttonnotification = (Button) findViewById(R.id.buttonnotification);
 
+
+
+    }
+    public void sn (View v){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder = new  Notification.Builder(NotificationActivity.this);
+        builder.setContentTitle("swipe@c4q.nyc");
+        builder.setContentText("Swipe right if you want to meet me. Otherwise, I'm not going away.");
+        builder.setSmallIcon(R.drawable.c4qfavicon);
+
+        Notification.Builder builder2 = new  Notification.Builder(NotificationActivity.this);
+        builder2.setContentTitle("permanent@c4q.nyc");
+        builder2.setContentText("I'm staying planted right here.");
+        builder2.setSmallIcon(R.drawable.c4qfavicon);
+        builder2.setOngoing(true);
+
+        Notification notification2 = builder2.build();
+        notificationManager.notify(1, notification2);
+
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
+
+    }
+    public void pn (View v){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder = new  Notification.Builder(NotificationActivity.this);
+        builder.setContentTitle("permanent@c4q.nyc");
+        builder.setContentText("I'm staying planted right here.");
+        builder.setSmallIcon(R.drawable.c4qfavicon);
+        builder.setOngoing(true);
+
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
+
+        Notification.Builder builder2 = new  Notification.Builder(NotificationActivity.this);
+        builder2.setContentTitle("permanent@c4q.nyc");
+        builder2.setContentText("I'm staying planted right here.");
+        builder2.setSmallIcon(R.drawable.c4qfavicon);
+        builder2.setOngoing(true);
+
+        Notification notification2 = builder2.build();
+        notificationManager.notify(1, notification2);
+
+    }
+    public void dp (View v){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
+    }
+    public void ac (View v){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder = new  Notification.Builder(NotificationActivity.this);
+        builder.setContentTitle("default@c4q.nyc");
+        builder.setContentText("Touch me to dismiss me!");
+        builder.setSmallIcon(R.drawable.c4qfavicon);
+        builder.setAutoCancel(true);
+
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
     }
 }
