c91738fba92561d4fb8060266bf125a6d497400b
jgarcia162 <jogarcia809@yahoo.com>
2015-06-28 12:54:16 -0400

    working on JSON

--- src/main/java/nyc/c4q/JSONActivity.java
+++ src/main/java/nyc/c4q/JSONActivity.java
@@ -2,21 +2,13 @@ package nyc.c4q;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 import nyc.c4q.json.Zipcode;
 
@@ -41,6 +33,14 @@ public class JSONActivity extends Activity {
         final TextView state = (TextView) findViewById(R.id.fieldstatevalue);
         final TextView _lat = (TextView) findViewById(R.id.fieldloclatvalue);
         final TextView _long = (TextView) findViewById(R.id.fieldloclongvalue);
+
+
+        Zipcode z = new Zipcode();
+
+        z.setId("11101");
+        z.setCity("ASTORIA");
+        z.setState("NY");
+        z.setPop(23142);
 
         addjson.setOnClickListener(new View.OnClickListener() {
             @Override
--- src/main/java/nyc/c4q/json/Zipcode.java
+++ src/main/java/nyc/c4q/json/Zipcode.java
@@ -1,4 +1,53 @@
 package nyc.c4q.json;
 
 public class Zipcode {
+    public String id;
+    public String city;
+    public String state;
+    public int pop;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPop() {
+        return pop;
+    }
+
+    public void setPop(int pop) {
+        this.pop = pop;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getField(String field){
+        if(field.equalsIgnoreCase("_id")){
+            return this.id;
+        }else if(field.equalsIgnoreCase("city")){
+            return this.city;
+        }else if(field.equalsIgnoreCase("state")){
+            return this.state;
+        }else if(field.equalsIgnoreCase("pop")){
+            return String.valueOf(this.pop);
+        }
+        else return null;
+    }
 }
