e62ed32031f82eade0f6d692e88943bdfa620138
tashsmit <tashsmit91@yahoo.com>
2015-06-28 12:58:58 -0400

    breaking for lunch

--- src/main/java/nyc/c4q/ListViewActivity.java
+++ src/main/java/nyc/c4q/ListViewActivity.java
@@ -2,6 +2,10 @@ package nyc.c4q;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ListViewActivity extends Activity {
@@ -19,11 +23,19 @@ public class ListViewActivity extends Activity {
             "#bf538d"
     };
     public TextView textLog;
+    public ListView list;
+    public ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
+
         textLog = (TextView) findViewById(R.id.textLog);
+        list = (ListView) findViewById(R.id.list);
+        adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_multiple_choice,
+                COLORS);
+        list.setAdapter(adapter);
     }
 }
